b476e484210d2f0512cd8c068abd1c350ab521a7
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-08-11 08:03:32 +0530

    Verify Schema added

---
+++ src/Schemas/VerifySchema.ts
@@ -0,0 +1,7 @@
+import { z } from 'zod'
+
+export const verifySchema = z.object({
+    code: z
+        .string({ message: "Invalid verificaton code" })
+        .length(6, "Varification code must be 6 digit")
+})
