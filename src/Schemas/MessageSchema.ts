325ee6ac76c486541b60b3572e506d4c172f8688
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-08-11 08:13:08 +0530

    Login , Message and AcceptMessage schema added

---
+++ src/Schemas/MessageSchema.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod'
+
+export const MessageSchema = z.object({
+    content: z
+        .string()
+        .min(8, { message: "Content must be at least 8 character" })
+        .max(1000, { message: "Content must not exceed 1000 characters" }),
+    createdAt: z.date()
+})
