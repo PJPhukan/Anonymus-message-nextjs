928d527516e529842e15e4fa25219a4b41a615c7
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-08-13 17:20:52 +0530

    Add connect ot data base

---
+++ src/lib/dbConnection.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number,
+}
+
+const connection: ConnectionObject = {}
+
+const dbConnect = async (): Promise<void> => {
+    if (connection.isConnected) {
+        console.log("Already connected to db.")
+        return
+    }
+    try {
+        const db = await mongoose.connect(process.env.MONGO_DB_URI || "")
+        console.log("Print DB :", db)
+
+        connection.isConnected = db.connections[0].readyState
+
+        console.log("DB connected successfully 🚀🚀")
+    } catch (error) {
+        console.log("Database connection failed", error)
+        process.exit(1)
+    }
+}
+export default dbConnect
